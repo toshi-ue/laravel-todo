234922c58c6d51bbe44ac3c372b019da1a2f1a3a
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-07 14:23:27 +0900

    bladeファイルでのvueの動作確認

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
+    {{-- こういう書き方もできる --}}
+    {{-- <title>{{ config('app.name', 'Ve')}}</title> --}}
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    @yield('wrapper')
+    <div id="app">
+        <example-component></example-component>
+        @yield('wrapper')
+    </div>
     {{-- TODO: BootStrap5 or TailWindCSSを追加する(CDN) --}}
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
 </html>
