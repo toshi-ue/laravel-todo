09581003220128c10ba7309af9ea7e95ebf56237
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-07 16:12:11 +0900

    タスク詳細コンポーネントを追加

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -19,7 +19,9 @@
           <td>Content1</td>
           <td>Ichiro</td>
           <td>
-            <button class="btn btn-primary">Show</button>
+            <router-link :to="{ name: 'task.show', params: { taskId: 1 } }">
+              <button class="btn btn-primary">Show</button>
+            </router-link>
           </td>
           <td>
             <button class="btn btn-success">Edit</button>
