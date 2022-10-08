10d626fd5cea1e07e1ea6506687b1addd9227151
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-08 16:04:03 +0900

    frontとapiを繋ぎ込み(タスク一覧取得)

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -4,59 +4,29 @@
       <thead class="thead-light">
         <tr>
           <th scope="col">#</th>
-          <th scope="col">Title</th>
-          <th scope="col">Content</th>
-          <th scope="col">Person In Charge</th>
+          <th scope="col">Description</th>
+          <th scope="col">Done</th>
+          <th scope="col">Created at</th>
           <th scope="col">Show</th>
           <th scope="col">Edit</th>
           <th scope="col">Delete</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Title1</td>
-          <td>Content1</td>
-          <td>Ichiro</td>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <th scope="row">{{ task.id }}</th>
+          <td>{{ task.description }}</td>
+          <td>{{ task.done }}</td>
+          <td>{{ task.created_at }}</td>
           <td>
-            <router-link :to="{ name: 'task.show', params: { taskId: 1 } }">
+            <router-link v-bind:to="{ name: 'task.show', params: { taskId: task.id } }">
               <button class="btn btn-primary">Show</button>
             </router-link>
           </td>
           <td>
-            <router-link :to="{ name: 'task.edit', params: { taskId: 1 } }">
+            <router-link v-bind:to="{ name: 'task.edit', params: { taskId: task.id } }">
               <button class="btn btn-success">Edit</button>
             </router-link>
-          </td>
-          <td>
-            <button class="btn btn-danger">Delete</button>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Title2</td>
-          <td>Content2</td>
-          <td>Jiro</td>
-          <td>
-            <button class="btn btn-primary">Show</button>
-          </td>
-          <td>
-            <button class="btn btn-success">Edit</button>
-          </td>
-          <td>
-            <button class="btn btn-danger">Delete</button>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Title3</td>
-          <td>Content3</td>
-          <td>Saburo</td>
-          <td>
-            <button class="btn btn-primary">Show</button>
-          </td>
-          <td>
-            <button class="btn btn-success">Edit</button>
           </td>
           <td>
             <button class="btn btn-danger">Delete</button>
@@ -68,5 +38,21 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    getTasks() {
+      axios.get("/api/tasks").then((res) => {
+        this.tasks = res.data;
+      });
+    },
+  },
+  mounted() {
+    this.getTasks();
+  },
+};
 </script>
