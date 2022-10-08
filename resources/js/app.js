e073e555bde47fc553b4fda946070829bccea0bd
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-08 11:41:16 +0900

    タスク登録コンポーネント実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 // import VueRouter from "vue-router";
 import VueRouter from 'vue-router';
 import HeaderComponent from "./components/HeaderComponent";
+import TaskCreateComponent from "./components/TaskCreateComponent";
 import TaskListComponent from "./components/TaskListComponent";
 import TaskShowComponent from "./components/TaskShowComponent";
 /**
@@ -36,6 +37,11 @@ const router = new VueRouter({
             path: '/tasks',
             name: 'task.list',
             component: TaskListComponent
+        },
+        {
+            path: '/tasks/create',
+            name: 'task.create',
+            component: TaskCreateComponent,
         },
         {
             path: '/tasks/:taskId',
