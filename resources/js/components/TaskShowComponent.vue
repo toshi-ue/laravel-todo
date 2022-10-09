526c3e022479ea73aa48ee0eb266b3ef2a945d2b
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-09 23:57:02 +0900

    タスク詳細コンポーネントを修正

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -10,40 +10,29 @@
               class="col-sm-9 form-control-plaintext"
               readonly
               id="id"
-              v-bind:value="taskId"
+              v-model="task.id"
             />
           </div>
           <div class="form-group row border-bottom">
-            <label for="title" class="col-sm-3 col-form-label">Title</label>
+            <label for="description" class="col-sm-3 col-form-label">Description</label>
             <input
               type="text"
               class="col-sm-9 form-control-plaintext"
               readonly
-              id="title"
-              value="title title"
+              id="description"
+              v-model="task.description"
             />
           </div>
           <div class="form-group row border-bottom">
-            <label for="content" class="col-sm-3 col-form-label">Content</label>
-            <input
-              type="text"
-              class="col-sm-9 form-control-plaintext"
-              readonly
-              id="content"
-              value="content content"
-            />
+            <label for="done" class="col-sm-3 col-form-label">Done</label>
+            <select name="done" id="done" class="form-control" v-model="task.done">
+              <option value="0" v-bind:selected="task.done">未完了</option>
+              <option value="1" v-bind:selected="task.done">完了</option>
+            </select>
           </div>
           <div class="form-group row border-bottom">
-            <label for="person-in-charge" class="col-sm-3 col-form-label"
-              >Person In Charge</label
-            >
-            <input
-              type="text"
-              class="col-sm-9 form-control-plaintext"
-              readonly
-              id="person-in-charge"
-              value="Ichiro"
-            />
+            <label for="created_at" class="col-sm-3 col-form-label">Created at</label>
+            {{ task.created_at }}
           </div>
         </form>
       </div>
@@ -54,6 +43,21 @@
 export default {
   props: {
     taskId: String,
+  },
+  data: function () {
+    return {
+      task: {},
+    };
+  },
+  methods: {
+    getTask() {
+      axios.get("/api/tasks/" + this.taskId).then((res) => {
+        this.task = res.data;
+      });
+    },
+  },
+  mounted() {
+    this.getTask();
   },
 };
 </script>
