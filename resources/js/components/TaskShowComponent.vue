28a1c7cd70dfbab7e5c39bba62264d64c90ab84a
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-12 13:36:41 +0900

    タスク詳細画面のデザインを修正

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -1,46 +1,54 @@
 <template>
-  <div class="container">
+  <div
+    class="container"
+    :style="'min-height: ' + $store.state.rect.contentMinHeight + 'px'"
+  >
     <div class="row justify-content-center">
       <div class="col-sm-6">
-        <form>
-          <div class="form-group row border-bottom">
-            <label for="id" class="col-sm-3 col-form-label">ID</label>
-            <input
-              type="text"
-              class="col-sm-9 form-control-plaintext"
-              readonly
-              id="id"
-              v-model="task.id"
-            />
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">TODO詳細</h4>
+            <div class="table-responsive">
+              <table class="table">
+                <tr>
+                  <th>ID</th>
+                  <td>{{ task.id }}</td>
+                </tr>
+                <tr>
+                  <th>詳細</th>
+                  <td>{{ task.description }}</td>
+                </tr>
+                <tr>
+                  <th>進捗</th>
+                  <td>
+                    <span v-if="task.done === '0'">未完了</span>
+                    <span v-else>完了</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th>作成日</th>
+                  <td>{{ task.created_at }}</td>
+                </tr>
+              </table>
+            </div>
+            <div class="text-right">
+              <router-link v-bind:to="{ name: 'task.edit', params: { taskId: task.id } }">
+                <button class="btn btn-success">変更</button>
+              </router-link>
+            </div>
           </div>
-          <div class="form-group row border-bottom">
-            <label for="description" class="col-sm-3 col-form-label">Description</label>
-            <input
-              type="text"
-              class="col-sm-9 form-control-plaintext"
-              readonly
-              id="description"
-              v-model="task.description"
-            />
-          </div>
-          <div class="form-group row border-bottom">
-            <label for="done" class="col-sm-3 col-form-label">Done</label>
-            <select name="done" id="done" class="form-control" v-model="task.done">
-              <option value="0" v-bind:selected="task.done">未完了</option>
-              <option value="1" v-bind:selected="task.done">完了</option>
-            </select>
-          </div>
-          <div class="form-group row border-bottom">
-            <label for="created_at" class="col-sm-3 col-form-label">Created at</label>
-            {{ task.created_at }}
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
+  // TODO:
+  //  一覧画面画面から当画面へ遷移してくると以下のエラーが発生する
+  //    [Vue warn]: Invalid prop: type check failed for prop "taskId". Expected String with value "6", got Number with value 6.
+  //  変更した後に詳細ページでリロードすると逆のエラーが発生する
+  //    [Vue warn]: Invalid prop: type check failed for prop "taskId". Expected Number with value 6, got String with value "6".
   props: {
     taskId: String,
   },
