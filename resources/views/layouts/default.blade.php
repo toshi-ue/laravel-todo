e3870826882537d32448ec959bb66f2fa2501de3
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-14 10:24:00 +0900

    FooterComponentを追加

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,17 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
-    {{-- こういう書き方もできる --}}
-    {{-- <title>{{ config('app.name', 'Ve')}}</title> --}}
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
     <div id="app">
         <header-component></header-component>
-        @yield('wrapper')
-        <router-view></router-view>
+        <router-view :key="$route.fullPath"></router-view>
+        <footer-component></footer-component>
     </div>
-    {{-- TODO: BootStrap5 or TailWindCSSを追加する(CDN) --}}
     <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
 </html>
