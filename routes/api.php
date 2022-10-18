2ef2c7239f99aa78a18a2ee578320f01217338d9
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-18 15:40:07 +0900

    ユーザー登録用APIを追加

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/register', 'Auth/RegisterController@register')->name('register');
+
 Route::get('/tasks', 'TaskController@index');
 Route::post('/tasks', 'TaskController@store');
 Route::get('/tasks/{task}', 'TaskController@show');
