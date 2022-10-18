7d61443bdbd99383f3d3d4b9e1c89077df5fe83f
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-18 21:41:30 +0900

    ユーザー登録、ログインフォームを追加

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -8,8 +8,33 @@
         {{ $t("signup") }}
       </li>
     </ul>
-    <div class="panel" v-show="tab === 1">Login Form</div>
-    <div class="panel" v-show="tab === 2">Register Form</div>
+    <div class="panel" v-show="tab === 1">
+      <form class="form" @submit.prevent="login">
+        <label for="login-mail">email</label>
+        <input type="text" class="form__item" id="login-mail" v-model="loginForm.email" />
+        <label for="password">password</label>
+        <input type="password" name="form__item" id="login-password" v-model="loginForm.password" />
+        <div class="form_button">
+          <button type="submit" class="button button-inverse">login</button>
+        </div>
+      </form>
+    </div>
+    <div class="panel" v-show="tab === 2">
+      <form class="form" @submit.prevent="register">
+        <label for="username">name</label>
+        <input type="text" class="form__item" id="username" v-model="registerForm.name" />
+        <label for="email">email</label>
+        <input type="text" class="form__item" id="email" v-model="registerForm.email" />
+        <label for="password">password</label>
+        <input type="password" name="form__item" id="password" v-model="registerForm.password" />
+        <label for="password-confirmation">password</label>
+        <input type="password" name="form__item" id="password-confirmation"
+          v-model="registerForm.password_confirmation" />
+        <div class="form_button">
+          <button type="submit" class="button button-inverse">register</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -18,7 +43,25 @@ export default {
   data() {
     return {
       tab: 1,
+      loginForm: {
+        email: '',
+        password: '',
+      },
+      registerForm: {
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+      },
     };
   },
+  methods: {
+    login() {
+      console.log(this.loginForm)
+    },
+    register() {
+      console.log(this.registerForm)
+    }
+  }
 };
 </script>
