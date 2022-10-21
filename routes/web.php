4537c69fb60961c4166b94e5ef2acb29d105628e
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-21 15:43:50 +0900

    Vue.jsのインストール、Laravelのログイン機能を追加
    php artisan ui vue --auth を実行

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('task', 'TaskController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
