76537780d4858c8bbde39224621a085e4267fa51
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-22 15:23:38 +0900

    vue-routerの設定、動作確認

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,15 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
+
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import router from "./router";
+
+window.Vue = Vue;
+Vue.use(VueRouter);
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +27,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +37,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
