4be90e8325a192c95ba7159f76f71de2ba2dfe25
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-22 20:46:49 +0900

    404ページを追加

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,10 +1,15 @@
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
+import NotFound from "./views/NotFound.vue";
 
 export default new Router({
     mode: "history",
     routes: [
+        {
+            path: '*',
+            component: NotFound
+        },
         {
             path: '/',
             name: 'home',
@@ -14,6 +19,6 @@ export default new Router({
             path: '/about',
             name: 'about',
             component: About
-        }
+        },
     ]
 })
