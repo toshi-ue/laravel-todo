cd61c97a71e6ebc0a06dd0c5b4500383a84c203a
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-10-22 16:22:55 +0900

    404エラーを回避するためのコード修正

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*');
 Route::resource('task', 'TaskController');
 
 Auth::routes();
