2b2635ee61c5132020d2284b0d845eee303753a9
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-11-23 21:20:22 +0900

    fontawesome-iconの設定を追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,17 @@ require('./bootstrap');
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Toasted from 'vue-toasted';
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPen, faTrash)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 import router from "./router";
 import store from "./store";
